b42fe88ad709884c8581886c3f33f6d30034b930
mbegley85 <146759083+mbegley85@users.noreply.github.com>
2023-10-02 12:30:24 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,52 @@
+// script.js
+const communalCard = document.getElementById('communal-card');
+const challengeTitle = document.getElementById('challenge-title');
+const challengePrompt = document.getElementById('challenge-prompt');
+const playerHand = document.getElementById('player-hand');
+
+const acceleratorCards = [
+    {
+        title: 'Terminator™',
+        description: 'Remove a critical part of a process or system and solve for its absence.',
+        example: 'To reduce wait time for premium members, the Hertz Gold Plus Rewards® loyalty program removes the waiting line altogether and allows drivers to go straight to their rental cars.',
+    },
+    // Add more accelerator cards...
+];
+
+let communalChallenge = null;
+let playerAccelerators = [];
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function createCard(cardText, containerId) {
+    const cardElement = document.createElement('div');
+    cardElement.classList.add('card');
+    cardElement.textContent = cardText;
+    document.getElementById(containerId).appendChild(cardElement);
+}
+
+function initializeGame() {
+    shuffle(acceleratorCards);
+
+    // Randomly select a communal challenge card
+    communalChallenge = challengeCards[Math.floor(Math.random() * challengeCards.length)];
+
+    // Display the communal challenge card
+    challengeTitle.textContent = communalChallenge.title;
+    challengePrompt.textContent = communalChallenge.prompt;
+
+    // Distribute accelerator cards to players (e.g., 5 cards each)
+    playerAccelerators = acceleratorCards.slice(0, 5);
+
+    // Display player's accelerator cards
+    playerAccelerators.forEach((accelerator) => {
+        createCard(accelerator.title, 'player-hand');
+    });
+}
+
+initializeGame();
